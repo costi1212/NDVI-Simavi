d7c637030d0fccd38e6ec7758131cc929acc24c3
Alexandru Turcu <alexandru.turcu2000@gmail.com>
2021-08-09 10:12:45 +0300

    polygon detection (not done)

--- contours.py
+++ contours.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from numpy.core.numeric import count_nonzero
+import random
 
 def extractPolygonCorners(imagePath, color):
     img = cv2.imread(imagePath)
@@ -18,11 +20,20 @@ def extractPolygonCorners(imagePath, color):
     dst = np.uint8(dst)
     ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-    corners = cv2.cornerSubPix(hsv, np.float32(centroids), (5, 5), (-1, -1), criteria)
+    #corners = cv2.cornerSubPix(hsv, np.float32(centroids), (5, 5), (-1, -1), criteria)
    # print("Corners "+imagePath + '\n')
     #for i in range(1, len(corners)):
      #   print(corners[i])
     img[dst > 0.1 * dst.max()] = [0, 0, 255]
+    
+    corners = []
+    #print("Corners")
+    tresh = dst > 0.1 * dst.max()
+    for i in range(dst.shape[0]):
+        for j in range(dst.shape[1]):
+            if (int(dst[i,j]) > tresh).all():
+                corners.append([j, i])
+
     cv2.imshow('corners', img)
     #cv2.waitKey(0)
     cv2.imwrite(f'Imagini/{color}points.png', img)
@@ -37,22 +48,56 @@ cv2.imshow('input image',image)
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 edged=cv2.Canny(gray,30,200)
-cv2.imshow('canny edges',edged)
-#cv2.waitKey(0)
 
 contours, hierarchy = cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 #cv2.imshow('canny edges after contouring', edged)
 #cv2.waitKey(0)
 
-print(contours)
-print('Numbers of contours found=' + str(len(contours)))
+#print(contours)
 
 corners = extractPolygonCorners(imagePath, "yellow")
-print(corners)
+#print(corners)
 
 #roundedCorners = np.rint(corners)
 #print(roundedCorners)
-# corner finder
+
+polygons = []
+
+for cont in contours:
+    poly = []
+    for coords in corners:
+        if coords in cont:
+            poly.append(coords)
+            #corners.remove(coords)
+
+    polygons.append(poly)
+
+#print(polygons)
+
+print('Numbers of contours found=' + str(len(contours)))
+print("Number of polygons:" + str(len(polygons)))
+
+'''
+for p in polygons:
+    b = random.randint(0,255)
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    for c in p:
+        image[c[1]][c[0]] = [b,r,g]
+'''
+
+for c in polygons[1]:
+    image[c[1]][c[0]] = [0,0,255]
+
+cv2.imshow("polygons", image)
+#'''
+cv2.drawContours(image,contours,-1,(255,0,255),3) 
+cv2.imshow('contours',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#'''
+
+
 '''
 gray = np.float32(gray)
 dst = cv2.cornerHarris(gray,2,3,0.04)
@@ -67,10 +112,3 @@ cv2.waitKey(0)
 
 #print(image[dst>0.01*dst.max()])
 '''
-
-#'''
-cv2.drawContours(image,contours,-1,(255,0,255),3) 
-cv2.imshow('contours',image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-#'''
