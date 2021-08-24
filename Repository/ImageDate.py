89a303338f5830c5961cc3526527fd46fc0e359f
costi1212 <viezurecostin@gmail.com>
2021-08-24 15:33:27 +0300

    Functionalitate pentru data ai date de test in Properties.py

---
+++ Repository/ImageDate.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+import requests
+from Properties.Properties import *
+import json
+from Repository.Conversions import *
+
+
+def postPixelCountService(polygonCoordinatesString, numberOfDays=30):
+    today = datetime.now()
+    urlPost = urlPixels + f'&startDate={((today - timedelta(days=numberOfDays)).strftime("%Y-%m-%d"))}' + f'&endDate={today.strftime("%Y-%m-%d")}'
+    polygonCoordinatesStringList = list(polygonCoordinatesString.strip().split(","))
+    polygonCoordinatesStringPairList = arrayToArrayOfPairs(polygonCoordinatesStringList)
+    polygonCoordinatesStringPairList.append(polygonCoordinatesStringPairList[0])
+    bodyJson = {"type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [polygonCoordinatesStringPairList]
+                }}
+    bodyJson = json.dumps(bodyJson)
+    response = requests.post(urlPost, data=bodyJson)
+    responseJson = response.json()
+    return responseJson
+
+
+def getOptimalDate(polygonCoordinatesString):
+    ok = 0
+    days = 30
+    max = 0
+    maxDate = 0
+    while ok == 0:
+        responseJson = postPixelCountService(polygonCoordinatesString, days)
+        maxDate = responseJson['results'][0]['date']
+        if responseJson['results'][0]['result']['average'] != 'NaN':
+            max = float(responseJson['results'][0]['result']['average'])
+        else:
+            max = 0
+        for i in range(len(responseJson['results'])):
+            #print('kok')
+            if responseJson['results'][i]['result']['average'] != 'NaN' and float(responseJson['results'][i]['result']['average']) > max:
+                max = float(responseJson['results'][i]['result']['average'])
+                maxDate =responseJson['results'][i]['date']
+        if max > 0.5:
+            ok = 1
+        days += 30
+    outputJson = {"average": max, "date": maxDate}
+    return json.dumps(outputJson)
+
+
